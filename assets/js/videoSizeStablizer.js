f6dad210dc1f9259e42c7334e9ca3b3df9885ce5
jplataquin <jp.lataquin@gmail.com>
2022-05-26 14:42:12 +0800

    updated

---
+++ assets/js/videoSizeStablizer.js
@@ -0,0 +1,34 @@
+export default function videoSizeStablizer(video){
+
+    const resizeObserver = new ResizeObserver(elems=>{
+        
+        console.log(elems);
+        
+        let elem = elems[0] ?? false;
+
+        if(!elem) return false;
+
+        let height  = window.innerHeight;
+        let width   = window.innerWidth;
+
+        console.log(width,height);
+
+        //Landscape
+        if(elem.offsetWidth > elem.offsetHeight){
+            console.log('landscape');
+            elem.style.width = width+'px';
+
+        }else if(elem.offsetWidth <= elem.offsetHeight){ //Portraint or Box
+            console.log('portrait');
+            elem.style.height = height+'px';
+            
+        }
+
+    });
+
+
+
+    resizeObserver.observe(video);
+
+    return resizeObserver;
+}
