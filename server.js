e9215e9a1895698b88cf4e5c2f022a28e4917b13
jplataquin <jp.lataquin@gmail.com>
2022-05-26 16:32:17 +0800

    updated

--- server.js
+++ server.js
@@ -240,12 +240,14 @@ io.sockets.on("connection", socket => {
     }
 
     
-    socket.on('ping',data,()=>{
+    socket.on('ping',(data)=>{
+        
         socket.to(data.id).emit('ping',{
             time:data.time,
             id:data.id,
             from:socket.id
         });
+        
     });
 
     /****************MOVEMENT*****************/
