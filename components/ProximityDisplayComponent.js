2ae2ded0f94f5347ff2c2bda79c494a440d7cee2
jplataquin <jp.lataquin@gmail.com>
2022-05-28 15:25:47 +0800

    updated:

--- components/ProximityDisplayComponent.js
+++ components/ProximityDisplayComponent.js
@@ -9,9 +9,18 @@ class ProximityDisplayComponent extends Component{
         return t.div(()=>{
 
             t.div({class:'main'},()=>{
-                t.div({class:'ball'});
-                t.div({class:'ball'});
-                t.div({class:'ball'});
+
+                t.div(()=>{
+                    t.div({class:'ball-row-1'});
+                    t.div({class:'ball-row-1'});
+                    t.div({class:'ball-row-1'});
+                });
+                
+                t.div(()=>{
+                    t.div({class:'ball-row-2'});
+                    t.div({class:'ball-row-2'});
+                    t.div({class:'ball-row-2'});
+                });
             });
         });
     }
@@ -28,11 +37,22 @@ class ProximityDisplayComponent extends Component{
                 width:'150px',
                 borderRadius:'50%'
             },
-            '.ball':{
+            '.ball-row-1':{
                 backgroundColor:'red',
                 height:'20px',
                 width:'20px',
-                borderRadius:'50%'
+                borderRadius:'50%',
+                float:'left',
+                marginLeft:'23px'
+            },
+
+            '.ball-row-2':{
+                backgroundColor:'red',
+                height:'20px',
+                width:'20px',
+                borderRadius:'50%',
+                float:'left',
+                marginLeft:'23px'
             }
         }
     }
