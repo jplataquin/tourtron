a49b8074dc12501e1c8dc209b96bcb7eb8f1b593
jplataquin <jp.lataquin@gmail.com>
2022-05-28 15:11:16 +0800

    updated:

---
+++ components/ProximityDisplayComponent.js
@@ -0,0 +1,29 @@
+import {Template, Component} from '../node_modules/adarna/dist/adarna.js';
+
+class ProximityDisplayComponent extends Component{
+
+    view(){
+
+        const t = new Template();
+
+        return t.div(()=>{
+
+            t.div({class:'main'});
+        });
+    }
+
+    controller(){
+
+    }
+
+    style(){
+        return {
+            '.main':{
+                backgroundColor:'rgba(0,0,0,0.2)',
+                height:'150px',
+                width:'150px',
+                borderRadius:'50%'
+            }
+        }
+    }
+}
