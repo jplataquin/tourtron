f47c5da8a93a894345f484f97ceb6d2008c081e2
jplataquin <jp.lataquin@gmail.com>
2022-05-29 22:17:51 +0800

    updated

---
+++ components/MainLayoutComponent.js
@@ -0,0 +1,40 @@
+import {Component,Template} from '/js/adarna.js';
+       
+class MainLayoutComponent extends Component {
+
+    model(){
+        return {
+            content: document.createElement('div')
+        }
+    }
+    view(){
+
+        const t = new Template();
+
+        return t.div(()=>{
+
+            t.nav({class:'navbar navbar-dark bg-dark'},()=>{
+                t.div({class:'container-fluid'},()=>{
+                    t.a({class:'navbar-brand',href:'/'},()=>{
+                        t.img({
+                            src:'https://via.placeholder.com/150',
+                            width:30,
+                            height:24,
+                            class:'d-inline-block align-text-top'
+                        });
+                        t.txt('Tourtron');
+                    });
+                });
+            });
+
+
+            t.div({class:'container'},()=>{
+                t.el(this.model.content);
+            });
+        });
+    }
+}
+
+export {
+    MainLayoutComponent
+}
